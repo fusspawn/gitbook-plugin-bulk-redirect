fa4d20983e9edd5b85af41d62bf74c60408a21a6
Paul Thornton <ariochofmelnibone@gmail.com>
2018-11-27 12:47:34 +0000

    force it to use the .html extension on files

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ var fs = require("fs");
 var content = function(path) {
   var s = "<!DOCTYPE HTML><html><head><meta charset='UTF-8'><title>Redirecting... Page moved</title>" +
         "<link rel='canonical' href='{}'><meta http-equiv=refresh content='0; url={:?}'></head>" +
-        "<body><h1>Redirecting... Page moved...</h1>" +
+        "<body>" +
         "<p><a href='{}'>Click here if you are not redirected</a></p>" +
         "<script>window.location.href='{}';</script>" +
         "</body></html>";
@@ -40,10 +40,10 @@ module.exports = {
       var g = this;
 
       conf.redirects.forEach(function (item) {
-        if (!item.from || !item.to) return;
-        var resolved = url.resolve(basepath, item.to);
-        g.output.writeFile(item.from, content(resolved));
-        g.log.debug("Redirect " + item.from + " -> " + resolved + "\n");
+          if (!item.from || !item.to) return;
+          var resolved = url.resolve(basepath, item.to);
+          g.output.writeFile(item.from + ".html", content(resolved));
+          g.log.debug("Redirect " + item.from + " -> " + resolved + "\n");
       });
     }
   }
